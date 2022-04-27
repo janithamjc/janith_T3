e156cc572da46219ad37c7c36d131c98e0830fa2
chandrakanthajanith <chandrakanthajnaith@gmail.com>
2022-04-27 06:33:55 -0700

    Mysql Sample 01

---
+++ Projects/Java-App13/src/MySqlDemo.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class MySqlDemo {
+
+    public static void main(String[] args) {
+
+        try {
+            Connection conn = myDriver.getConnection();
+           ResultSet rs =conn.createStatement().
+                   executeQuery("select * from users");
+            while (rs.next()){
+                int id = rs.getInt("idUser");
+                String name = rs.getString("name");
+                System.out.println(id+" "+name);
+            }
+            conn.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
